7b781f69fb81e12c2fcbfd943fab42eb5453f829
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-05-28 13:32:26 -0300

    Get and Put 'isDark' from shared preferences

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/foundation.dart';
 
 import './models/app_config_model.dart';
+import './interfaces/local_storage_interface.dart';
+import './services/shared_local_storage_service.dart';
 
 class ThemeController {
   static ThemeController instance = ThemeController._();
 
-  ThemeController._();
+  ThemeController._() {
+    storage.get('isDark').then((value) {
+      if (value != null) {
+        this.changeTheme(value);
+      }
+    });
+  }
 
   final config = AppConfigModel();
   bool get isDark => this.config.themeSwitcher.value;
   ValueNotifier<bool> get themeSwitcher => this.config.themeSwitcher;
 
+  final ILocalStorage storage = SharedLocalStorageService();
+
   changeTheme(bool value) {
     this.themeSwitcher.value = value;
+
+    storage.put('isDark', value);
   }
 }
